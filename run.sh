1a99c9471742dc88db3daec471469863009135a9
Yi Guo <yi.guo@yahoo.com>
2015-03-14 18:26:24 -0400

    Upload makefile and run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear
+
+file="Main.class"
+
+if [ ! -f $file ]; then
+	echo "Compiling..."
+	make
+	echo ""
+fi
+
+java -classpath .:org-apache-commons-codec.jar Main "$1" "$2" "$3"
